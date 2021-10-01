cf01a8e1a33187c17777c01ed4fdfa5f4e522c50
AMARARENE <zina.amararene@gmail.com>
2021-10-01 14:57:50 +0200

    Product management, product entity creation, category and search form

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Controller;
+
+use App\Classe\Search;
+use App\Entity\Product;
+use App\Form\SearchType;
+use App\Repository\ProductRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProductController extends AbstractController
+{
+    /**
+     * @Route("/products", name="products")
+     */
+    public function productsList(ProductRepository $productRepository, Request $request): Response
+    {
+        $search = new Search();
+
+        $form = $this->createForm(SearchType::class, $search);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $products = $productRepository->findWithSearch($search);
+        } else {
+            $products = $productRepository->findAll();
+        }
+
+        return $this->render('product/products.html.twig', [
+            'products' => $products,
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/product/{slug}", name="product")
+     */
+    public function show($slug, ProductRepository $productRepository)
+    {
+        $product = $productRepository->findOneBySlug($slug);
+
+        if (!$product) {
+            return $this->redirectToRoute('products');
+        }
+
+        return $this->render('product/show.html.twig', [
+            'product' => $product
+        ]);
+    }
+}
---
+++ src/Form/SearchType.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Form;
+
+use App\Classe\Search;
+use App\Entity\Category;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SearchType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('string', TextType::class, [
+                'label' => false,
+                'required' => false,
+                'attr' => [
+                    'placeholder' => 'Votre recherche ...',
+                    'class' => 'form-control-sm'
+                ]
+            ])
+            ->add('categories', EntityType::class, [
+                'class' => Category::class,
+                'label' => false,
+                'required' => false,
+                'multiple' => true, // selectionner plusieurs valeurs
+                'expanded' => true // avoir une vue en checkbox
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Filtrer',
+                'attr' => [
+                    'class' => 'btn-block btn-info'
+                ]
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Search::class,
+            'method' => 'GET',
+            'crsf_protection' => false
+        ]);
+    }
+
+    public function getBlockPrefix()
+    {
+        return '';
+    }
+}
